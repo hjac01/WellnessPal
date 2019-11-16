286d4fc2fbdfa1c6b8055b5b54660e4862f9228a
Hannah Jacoby <hjacoby@grubhub.com>
2019-11-16 12:00:08 -0700

    updates

--- client/src/components/main/index.js
+++ client/src/components/main/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Switch, Route} from 'react-router-dom';
-import Landing from '../landing';
-import Profile from '../profile'
-import Login from '../login';
-import CreateAcount from '../createAcoutn';
+import Landing from '../Landing/index';
+import Profile from '../Profile/index';
+import Login from '../Login/index';
+import Articles from '../Articles/index'; 
+import CreateAccount from '../CreateAccount/index';
 
 const Main = () => (
     <Switch> 
         <Route exact path='/' component={Landing} />
         <Route path='/profile' component={Profile} />
         <Route path='/login' component={Login} />
-        <Route path='/createAcount' component={CreateAcount} />
-        <Route path='/resolve' components={ArticleResults} />
+        <Route path='/CreateAccount' component={CreateAccount} />
+        <Route path='/results' components={Articles} />
     </Switch>
 )
 
