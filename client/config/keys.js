5dde53b7f75d252fdaa7a55c2dd76889946ab85d
hjac01 <51024035+hjac01@users.noreply.github.com>
2019-11-23 15:39:14 -0700

    Create keys.js

---
+++ client/config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: "696079218005-lbg6gjp2c9lpjg0ek30afiaj6td2t96n.apps.googleusercontent.com",
+    googleClientSecret: "7cVbbl64EcNgKVCateCz5nIV",
+    mongoURI: "mongodb+srv://Wellnesspal:<Classwork1234>@cluster0-rpqub.gcp.mongodb.net/test?retryWrites=true&w=majority",
+    cookieKey: "fnkds;lanksdf;aosdnf;asoddfin"
+   };
